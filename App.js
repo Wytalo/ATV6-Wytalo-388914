c481dac91ff56d998ab3b49b9657b40b9110207f
Wytalo <31418745+Wytalo@users.noreply.github.com>
2021-03-10 16:01:54 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { FirebaseApp } from './components/firebaseApp';
+
+const App = () => {
+  return <FirebaseApp />;
+};
+
+export default App;
